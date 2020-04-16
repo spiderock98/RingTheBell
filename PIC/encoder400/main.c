fe0da78caf16eeb52aae9c4baefbd147a4a664f6
spiderock98 <nguyen.minhtien98@gmail.com>
2020-04-16 11:41:40 -0700

    - ISR_TIMER2 >> ngat neu vuot qua 4 vong - ISR_TIMER2 >> polling increase and decrease button

--- PIC/encoder400/main.c
+++ PIC/encoder400/main.c
@@ -2,28 +2,36 @@
 #include "def_lcd.c"
 #include "floor.c"
 
-#define triac1 RC6
-// new
+/***************************************************** PREPROSECSOR ****************************************************/
+
+// 1 >> on
 #define triac1Out PIN_C6
-
-#define triac2 RC7
-// new
 #define triac2Out PIN_C7
-
-#define relay RC1
-// new
 #define relayOut PIN_C1
+// 0 >> pressed
+#define btnINCREASE PIN_B4
+#define btnDECREASE PIN_B1
+// 1 >> led on
+#define ledSAFETY PIN_D1
+#define ledSTARTING PIN_D2
+#define ledRINGING PIN_C5
+#define ledBUTTON PIN_D4
+
+/***************************************************** VARIABLES *****************************************************/
 
 // set angle (10 levels). more angle, weaker motor
-float32 angleStarter = 1;
-float32 angleRingTheBell = 6;
+volatile int8 angleStarter = read_eeprom(0x00);
+volatile int8 angleRingTheBell = read_eeprom(0x01);
 
-int32 valTimer0SetStarter = (int32)FLOOR((13.1072 - angleStarter) / 0.0512) - 1;
-int32 valTimer0SetRingTheBell = (int32)FLOOR((13.1072 - angleRingTheBell) / 0.0512) - 1;
+volatile int32 valTimer0SetStarter = (int32)FLOOR((13.1072 - angleStarter) / 0.0512) - 1;
+volatile int32 valTimer0SetRingTheBell = (int32)FLOOR((13.1072 - angleRingTheBell) / 0.0512) - 1;
 
 volatile signed int16 count = 0;
 int8 countTime = 0, anglePercent = 0;
+int16 iTimer2OverFlow;
 int1 flagForward = true, flagStarter = true, flagSTOP = true;
+
+/***************************************************** ISR Func() *****************************************************/
 
 #byte CMCON = 0x9C
 #INT_COMP
@@ -64,6 +72,72 @@ void timer0_isr()
    disable_interrupts(INT_TIMER0);
 }
 
+#INT_TIMER2
+void timer2_isr()
+{
+   if (!(--iTimer2OverFlow))
+   {
+      // reset neu quay qua 4 vong
+      if (flagForward)
+      {
+         if (count >= 1600) // (+)4 rotate
+         {
+            output_low(relayOut); // safety switch
+            //TODO: xuat tin hieu reset
+         }
+      }
+      else
+      {
+         if (count <= -1600) // (-)4 rotate
+         {
+            output_low(relayOut); // safety switch
+            //TODO: xuat tin hieu reset
+         }
+      }
+
+      if (!btnDECREASE)
+      {
+         if (flagStarter)
+         {
+            if (angleStarter < 9)
+            {
+               write_eeprom(0x00, ++angleStarter);
+               valTimer0SetStarter = (int32)FLOOR((13.1072 - angleStarter) / 0.0512) - 1;
+            }
+         }
+         else
+         {
+            if (angleRingTheBell < 9)
+            {
+               write_eeprom(0x01, ++angleRingTheBell);
+               valTimer0SetRingTheBell = (int32)FLOOR((13.1072 - angleRingTheBell) / 0.0512) - 1;
+            }
+         }
+      }
+      else if (!btnINCREASE)
+      {
+         if (flagStarter)
+         {
+            if (angleStarter > 1)
+            {
+               write_eeprom(0x00, --angleStarter);
+               valTimer0SetStarter = (int32)FLOOR((13.1072 - angleStarter) / 0.0512) - 1;
+            }
+         }
+         else
+         {
+            if (angleRingTheBell > 1)
+            {
+               write_eeprom(0x01, --angleRingTheBell);
+               valTimer0SetRingTheBell = (int32)FLOOR((13.1072 - angleRingTheBell) / 0.0512) - 1;
+            }
+         }
+      }
+
+      iTimer2OverFlow = 7660; // 100ms every command
+   }
+}
+
 #INT_CCP1
 void ccp1_isr()
 {
@@ -73,6 +147,8 @@ void ccp1_isr()
       --count;
    // clear_interrupt(INT_CCP1);
 }
+
+/***************************************************** FUNC() *****************************************************/
 
 void FORWARD()
 {
@@ -178,32 +254,31 @@ void checkSafetyFirst(int32 sec)
    count = 0; // update 0 point
    output_high(relayOut);
    delay_ms(3000); // ngăn hồ quang nếu cùng lúc đóng triac lập tức
+   enable_interrupts(INT_TIMER2);
    flagSTOP = false;
 }
 
+/***************************************************** MAIN ********************************************************/
+
 void main()
 {
-   // initLCD();
-
-   // ghima(0x01); // clear
-   // ghima(0x80); // set 0,0
-   // hienthi(arrNumber[count / 10]);
-   // hienthi(arrNumber[count % 10]);
-
-   TRISB0 = TRISC0 = TRISC2 = 1; //input
-   TRISC6 = TRISC7 = TRISC1 = 0; //output
+   TRISB0 = TRISC0 = TRISC2 = TRISB1 = TRISB4 = 1;                   //input
+   TRISC6 = TRISC7 = TRISC1 = TRISD1 = TRISD2 = TRISC5 = TRISD4 = 0; //output
 
    clear_interrupt(INT_EXT);
    enable_interrupts(INT_EXT);
    ext_int_edge(H_TO_L);
 
+   setup_timer_0(RTCC_INTERNAL | RTCC_DIV_256); // 51.2us
+   enable_interrupts(INT_TIMER0);
+
    setup_ccp1(CCP_CAPTURE_FE);
    setup_timer_1(T1_INTERNAL);
    enable_interrupts(INT_CCP1);
 
-   setup_timer_0(RTCC_INTERNAL | RTCC_DIV_256); //51.2us
-   // set_timer0(59);                             //10.086ms
-   enable_interrupts(INT_TIMER0);
+   setup_timer_2(T2_DIV_BY_16, 255, 16); // Overflow every 13.056us
+   set_timer2(0);
+   iTimer2OverFlow = 7660; // 100ms every command
 
    setup_comparator(A0_VR_A1_VR);
    setup_vref(VREF_HIGH | 5);
--- PIC/encoder400/main.h
+++ PIC/encoder400/main.h
@@ -1,7 +1,7 @@
 #include <16F877A.h>
 #include <def_16f877a.h>
 
-#device ADC = 16
+#device ADC = 16, WRITE_EEPROM=ASYNC
 
 #FUSES NOWDT      //No Watch Dog Timer
 #FUSES NOBROWNOUT //No brownout reset
